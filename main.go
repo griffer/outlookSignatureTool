2992baba45e8b54128d579750f3ce60d030ce2a3
Anders Jørgensen <1442608+griffer@users.noreply.github.com>
2019-10-17 10:49:34 +0200

    Added databaseReadSignatures function

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 	"os/user"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -16,6 +19,7 @@ func main() {
 	outlookDataPath := user.HomeDir + "/Library/Group Containers/UBF8T346G9.Office/Outlook/Outlook 15 Profiles/Main Profile/Data"
 	outlookDatabasePath := outlookDataPath + "/Outlook.sqlite"
 	databaseCheckIfExists(outlookDatabasePath)
+	databaseReadSignatures(outlookDatabasePath)
 }
 
 // Check if the Outlook database is present
@@ -26,4 +30,20 @@ func databaseCheckIfExists(outlookDatabasePath string) {
 			panic(err)
 		}
 	}
+}
+
+// databaseReadSignatures returns a slice of all signatures configured in outlook
+func databaseReadSignatures(outlookDatabasePath string) []string {
+	var signatureSlice []string
+	database, _ := sql.Open("sqlite3", outlookDatabasePath)
+	rows, _ := database.Query("SELECT Record_RecordID, PathToDataFile FROM Signatures")
+	var PathToDataFile string
+	var RecordID string
+	for rows.Next() {
+		rows.Scan(&RecordID, &PathToDataFile)
+		// For now we combine the RecordID field from the database with the PathToDataFile field
+		signatureSlice = append(signatureSlice, RecordID+"/"+PathToDataFile)
+	}
+	fmt.Printf("%v", signatureSlice)
+	return signatureSlice
 }
